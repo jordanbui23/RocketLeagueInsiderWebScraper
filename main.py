b42e4d0b60c0749e7892de690f0f3251b8f36cc1
jordanbui23 <jordanbui20@gmail.com>
2021-11-18 16:10:42 -0600

    Added more comments, Formatted the comments nicely.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ client = Client(account_sid, auth_token)
 ##############################################################
 
 # Holds the links for all of the items I want to keep track of
+# The links in the list are some of the items I currently own
 links = ["https://rl.insider.gg/en/xbox/octane/white",
          "https://rl.insider.gg/en/xbox/fennec/white",
          "https://rl.insider.gg/en/xbox/interstellar/black",
@@ -21,22 +22,22 @@ links = ["https://rl.insider.gg/en/xbox/octane/white",
 
 def find_name_price(link):
     html_text = requests.get(link).text
-    soup = BeautifulSoup(html_text, "lxml")  # Holds the lxml parsed html
-    general = soup.find("tr", id="matrixRow0")  # The matrix row holds prices for all consoles
-    rows = general.find_all("td")  # Finds all of the prices in the rows
-    price = rows[3].text  # The fourth item in the list is xbox price
-    box = soup.find("div", id="sellerOffersContainer")  # Finds the container holding the name of the item
-    name = str(box.h2.text)  # The name is held in here, but needs more formatting
-    # [H] Titanium White Octane [W] Credits
+    soup = BeautifulSoup(html_text, "lxml")              # Holds the lxml parsed html
+    general = soup.find("tr", id="matrixRow0")           # The matrix row holds prices for all consoles
+    rows = general.find_all("td")                        # Finds all of the prices in the rows
+    price = rows[3].text                                 # The fourth item in the list is xbox price
+    box = soup.find("div", id="sellerOffersContainer")   # Finds the container holding the name of the item
+    name = str(box.h2.text)                              # The name is held in here, but needs more formatting
+    # name is currently formatted like "[H] Titanium White Octane [W] Credits"
     name = name.replace("[H]", "")
     name = name.replace("[W]", "")
     name = name.replace("Credits", "")
-    name = name.strip()  # Strip the white space from the name
-    client.messages.create(body=f"The price for {name} today is {price} credits", from_='+1', to='+1')
+    name = name.strip()                                  # Strip the white space from the name
+    client.messages.create(body=f"The price for {name} today is {price} credits", from_='+19139149044', to='+19132656747')
 
 
 if __name__ == "__main__":
     while True:
-        for item in links:  # Loops through all of the items in the links array
+        for item in links:                               # Loops through all of the items in the links array
             find_name_price(item)
-        time.sleep(10)  # Time in seconds until it runs the method again
+        time.sleep(1000)                                 # Time in seconds until it runs the method again
