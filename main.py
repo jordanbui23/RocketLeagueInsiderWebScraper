a9f8a2750994034358ddb651e02ccd059e8ff5c3
jordanbui23 <jordanbui20@gmail.com>
2021-11-23 11:59:28 -0600

    Changed it so that it sends one formatted text instead of n amount of texts

--- main.py
+++ main.py
@@ -20,24 +20,26 @@ links = ["https://rl.insider.gg/en/xbox/octane/white",
          ]
 
 
-def find_name_price(link):
-    html_text = requests.get(link).text
-    soup = BeautifulSoup(html_text, "lxml")              # Holds the lxml parsed html
-    general = soup.find("tr", id="matrixRow0")           # The matrix row holds prices for all consoles
-    rows = general.find_all("td")                        # Finds all of the prices in the rows
-    price = rows[3].text                                 # The fourth item in the list is xbox price
-    box = soup.find("div", id="sellerOffersContainer")   # Finds the container holding the name of the item
-    name = str(box.h2.text)                              # The name is held in here, but needs more formatting
-    # name is currently formatted like "[H] Titanium White Octane [W] Credits"
-    name = name.replace("[H]", "")
-    name = name.replace("[W]", "")
-    name = name.replace("Credits", "")
-    name = name.strip()                                  # Strip the white space from the name
-    client.messages.create(body=f"The price for {name} today is {price} credits", from_='+19139149044', to='+19132656747')
+def find_price():
+    string = ""                                              # Holds an empty string that will be formatted
+    for item in links:
+        html_text = requests.get(item).text                  # Holds the html prior to being parsed
+        soup = BeautifulSoup(html_text, "lxml")              # Holds the lxml parsed html
+        general = soup.find("tr", id="matrixRow0")           # The matrix row holds prices for all consoles
+        rows = general.find_all("td")                        # Finds all of the prices in the rows
+        price = rows[3].text                                 # The fourth item in the list is xbox price
+        box = soup.find("div", id="sellerOffersContainer")   # Finds the container holding the name of the item
+        name = str(box.h2.text)                              # The name is held in here, but needs more formatting
+        # name is currently formatted like "[H] Titanium White Octane [W] Credits"
+        name = name.replace("[H]", "")
+        name = name.replace("[W]", "")
+        name = name.replace("Credits", "")
+        name = name.strip()                                  # Strip the white space from the name
+        string += f"The price for {name} today is {price} credits\n"
+    client.messages.create(body=string, from_='+19139149044', to='+19132656747')
 
 
 if __name__ == "__main__":
     while True:
-        for item in links:                               # Loops through all of the items in the links array
-            find_name_price(item)
-        time.sleep(1000)                                 # Time in seconds until it runs the method again
+        find_price()                                         # Calls the function
+        time.sleep(1000)                                     # Time in seconds until it runs the method again
